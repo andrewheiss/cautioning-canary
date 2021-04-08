93c0e1ca04a24df949b3af709249095e8bd7ff5a
Andrew Heiss <andrew@andrewheiss.com>
2021-04-08 08:47:22 -0400

    Add word count stuff as function instead of external script

--- R/funs_knitting.R
+++ R/funs_knitting.R
@@ -1,4 +1,7 @@
 library(bib2df)
+library(rvest)
+library(xml2)
+library(stringi)
 
 render_html <- function(input, output, csl, support_folder, ...) {
   # Add CSS file as a dependency so it goes into the _files directory
@@ -148,4 +151,60 @@ extract_bib <- function(input_rmd, input_bib, output, ...) {
            -starts_with("DATE."), -KEYWORDS) %>%
     arrange(CATEGORY, BIBTEXKEY) %>%
     df2bib(output)
+}
+
+
+# string::stri_count_words() counts, um, words. That's its whole job. But it
+# doesn't count them like Microsoft Word counts them, and academic publishing
+# portals tend to care about Word-like counts. For instance, Word considers
+# hyphenated words to be one word, while stringr counts them as two (and even
+# worse, stringi counts / as word boundaries, so URLs can severely inflate your
+# word count).
+#
+# Also, academic writing typically doesn't count the title, abstract, table
+# text, figure captions, or equations as words in the manuscript (and it
+# SHOULDN'T count bibliographies, but it always seems to ugh).
+#
+# This parses the rendered HTML, removes extra elements, adjusts the text so
+# that stringi treats it more like Word, and then finally provides a more
+# accurate word count.
+count_words <- function(html) {
+  # Read the HTML file
+  ms_raw <- read_html(html)
+
+  # Extract just the article, ignoring title, abstract, etc.
+  ms <- ms_raw %>%
+    html_nodes("article")
+
+  # Get rid of figures, tables, and math
+  xml_remove(ms %>% html_nodes("figure"))
+  xml_remove(ms %>% html_nodes("table"))
+  xml_remove(ms %>% html_nodes(".display"))  # Block math
+  xml_replace(ms %>% html_nodes(".inline"), read_xml("<span>MATH</span>")) %>%
+    invisible()
+
+  # Go through each child element in the article and extract it
+  ms_cleaned_list <- map(html_children(ms), ~ {
+    .x %>%
+      html_text(trim = TRUE) %>%
+      # ICU counts hyphenated words as multiple words, so replace - with DASH
+      str_replace_all("\\-", "DASH") %>%
+      # ICU also counts / as multiple words, so URLs go crazy. Replace / with SLASH
+      str_replace_all("\\/", "SLASH") %>%
+      # ICU *also* counts [things] in brackets multiple times, so kill those too
+      str_replace_all("\\[|\\]", "") %>%
+      # Other things to ignore
+      str_replace_all("×", "")
+  })
+
+  # Get count of words! (close enough to Word)
+  final_count <- sum(stri_count_words(ms_cleaned_list))
+
+  class(final_count) <- c("wordcount", "numeric")
+
+  return(final_count)
+}
+
+print.wordcount <- function(x) {
+  cat(scales::comma(x), "words in manuscript\n")
 }
